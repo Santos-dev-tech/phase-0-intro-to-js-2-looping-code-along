9945ca92f9b7b599b896791eef27f742a2e3b910
Zain <zainsantos21@gmail.com>
2025-03-14 09:12:52 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-// Code your solutions in this file
-// writeCards() - returns an array of thank you messages
+
 function writeCards(names, occasion) {
     return names.map(name => {
       return `Thank you, ${name}, for the wonderful ${occasion} gift!`;
     });
   }
   
-  // countDown() - logs numbers starting from the given number down to 0
+  
   function countDown(start) {
     for (let i = start; i >= 0; i--) {
       console.log(i);
